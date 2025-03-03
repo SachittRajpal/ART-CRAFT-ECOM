fee1e805bcbf96f0e737f084e604ac6ac87971bc
SachittRajpal <sachit1328.be22@chitkarauniversity.edu.in>
2025-03-03 20:18:11 +0530

    Update role_permissions.js

--- models/role_permissions.js
+++ models/role_permissions.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../configs/database');
 const Role = require('./roles'); // Import the Role model
-const Permissions = require('./permissions'); // Import the Permissions model
+
 
 const Role_Permissions = sequelize.define('Role_Permissions', {
   roleid: {
@@ -14,8 +14,6 @@ const Role_Permissions = sequelize.define('Role_Permissions', {
   },
 });
 
-// Associations
-Role_Permissions.belongsTo(Role, { foreignKey: 'roleid', as: 'role' }); // Role-Permissions relationship
-Role_Permissions.belongsTo(Permissions, { foreignKey: 'permissionsid', as: 'permissions' }); // Permissions-Role relationship
+
 
 module.exports = Role_Permissions;
