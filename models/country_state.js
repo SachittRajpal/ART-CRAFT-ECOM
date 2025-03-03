c9d94538b51b0b701754a68ab600fcce869faa12
SachittRajpal <sachit1328.be22@chitkarauniversity.edu.in>
2025-03-03 19:45:45 +0530

    Update country_state.js

--- models/country_state.js
+++ models/country_state.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../configs/database');
-const Country = require('./country'); // Assuming the Country model is in models/country.js
-const State = require('./state'); // Assuming the State model is in models/state.js
+const Country = require('./country'); 
+const State = require('./state');
 
 const Country_State = sequelize.define('Country_State', {
     countryid: {
@@ -13,11 +13,11 @@ const Country_State = sequelize.define('Country_State', {
         allowNull: false
     }
 }, {
-    tableName: 'country_state', // Optional, this makes the table name explicit
-    timestamps: false // Set to true if you want to use createdAt/updatedAt fields
+    tableName: 'country_state', 
+    timestamps: false 
 });
 
-// Define the associations
+
 
 // Many-to-many relationship: A country can have many states, and a state can belong to many countries.
 Country.belongsToMany(State, { through: Country_State, foreignKey: 'countryid' });
